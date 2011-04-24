de91eb0c27fb296c06df88c662ad11ed836c5cc1
mondalaci <mondalaci@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-24 11:27:05 +0000

    Provide instructions for directly inserting the content of ultimate-flash-xhtmlizer.{css, js} into the theme.  Wrap long lines.
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-flash-xhtmlizer/trunk@376538 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ultimate-flash-xhtmlizer.php
+++ ultimate-flash-xhtmlizer.php
@@ -19,20 +19,22 @@ function ufx_replace_flash_object($matches)
 
 function ufx_replace_flash_objects($html)
 {
-    return preg_replace_callback('/(<object[ \t\n]+.*>.*<\/object>)/i', 'ufx_replace_flash_object', $html);
+    return preg_replace_callback('/(<object[ \t\n]+.*>.*<\/object>)/i',
+                                 'ufx_replace_flash_object', $html);
 }
 
 $options = get_option('ufx_options');
 if (isset($options['do_include_css_and_js'])) {
     wp_register_style('ultimate-flash-xhtmlizer.css', UFX_PLUGIN_URL . 'ultimate-flash-xhtmlizer.css');
     wp_enqueue_style('ultimate-flash-xhtmlizer.css');
-    wp_register_script('ultimate-flash-xhtmlizer.js', UFX_PLUGIN_URL . 'ultimate-flash-xhtmlizer.js', array('jquery'));
+    wp_register_script('ultimate-flash-xhtmlizer.js', UFX_PLUGIN_URL . 'ultimate-flash-xhtmlizer.js',
+                       array('jquery'));
     wp_enqueue_script('ultimate-flash-xhtmlizer.js');
 }
 
 add_filter('the_content', 'ufx_replace_flash_objects');
 
-//////////////////////////////////////// Option handling ////////////////////////////////////////
+///////////////////////////////////////// Option handling /////////////////////////////////////////
 
 function ufx_add_option_page()
 {
@@ -52,28 +54,41 @@ function ufx_add_option_page()
 
 function ufx_admin_menu()
 {
-    add_options_page('Ultimate Flash XHTMLizer', 'Ultimate Flash XHTMLizer', 'manage_options', 'ufx_options', 'ufx_add_option_page');
+    add_options_page('Ultimate Flash XHTMLizer', 'Ultimate Flash XHTMLizer', 'manage_options',
+                     'ufx_options', 'ufx_add_option_page');
 }
 
 function ufx_section_text()
 {
-    print "You may don't want to include (the very small) CSS and JavaScript code from external " .
-          "files and rather directly include their content to your theme to decrease the number " .
-          "of HTTP requests and speed up your site.  If so, uncheck the next option.";
+    $plugin_directory = get_bloginfo('wpurl') . '/wp-content/plugins/ultimate-flash-xhtmlizer';
+
+    print "You may not want to include (the very small) CSS and JavaScript code from external " .
+          "files and rather directly insert their content into your theme to decrease the number " .
+          "of HTTP requests and speed up your site.<br /><br />" .
+          "To do so:<br />" .
+          "1) Put the content of <a href=\"$plugin_directory\ultimate-flash-xhtmlizer.css\" " .
+          "target=\"_blank\">ultimate-flash-xhtmlizer.css</a> into the stylesheet of your blog.<br />" .
+          "2) Put the content of <a href=\"$plugin_directory\ultimate-flash-xhtmlizer.js\" " .
+          "target=\"_blank\">ultimate-flash-xhtmlizer.js</a> into the template of your blog, " .
+          "directly before &lt;/body&gt;, wrapped inside &lt;script&gt; and &lt;/script&gt;tags. " .
+          "Also, don't forget to include jQuery which is utilized by this script.<br />" .
+          "3) Uncheck the following option.";
 }
 
 function ufx_print_option()
 {
     $options = get_option('ufx_options');
     $checked = isset($options['do_include_css_and_js']) ? 'checked="checked"' :'';
-    print '<input id="do_include_css_and_js" type="checkbox" name="ufx_options[do_include_css_and_js]" ' . $checked . '" />';
+    print '<input id="do_include_css_and_js" type="checkbox" ' .
+          'name="ufx_options[do_include_css_and_js]" ' . $checked . '" />';
 }
 
 function ufx_admin_init()
 {
     register_setting('ufx_options', 'ufx_options');
     add_settings_section('ufx_main', 'Main Settings', 'ufx_section_text', __FILE__);
-    add_settings_field('do_include_css_and_js', 'Include CSS and JavaScript', 'ufx_print_option', __FILE__, 'ufx_main');
+    add_settings_field('do_include_css_and_js', 'Include CSS and JavaScript',
+                       'ufx_print_option', __FILE__, 'ufx_main');
 }
 
 function ufx_add_defaults() {
